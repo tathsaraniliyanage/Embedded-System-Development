d57d355618842f341c2c3314deef78001dd100ad
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-10-10 18:14:58 +0530

    Niterider Light system created

---
+++ niterider lights/niterider.ino
@@ -0,0 +1,79 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+   pinMode(9, OUTPUT);
+   pinMode(7, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(3, OUTPUT);
+}
+
+
+
+void loop()
+    {
+        digitalWrite(13, HIGH);
+        delay(5); 
+        digitalWrite(13, LOW);
+        delay(5); 
+        
+        digitalWrite(11, HIGH);
+        delay(5); 
+        digitalWrite(11, LOW);
+        delay(5); 
+        
+        digitalWrite(9, HIGH);
+        delay(5); 
+        digitalWrite(9, LOW);
+        delay(5); 
+        
+        digitalWrite(7, HIGH);
+        delay(5); 
+        digitalWrite(7, LOW);
+        delay(5); 
+        
+        digitalWrite(5, HIGH);
+        delay(5); 
+        digitalWrite(5, LOW);
+        delay(5); 
+        
+        digitalWrite(3, HIGH);
+        delay(5); 
+        digitalWrite(3, LOW);
+        delay(5); 
+        
+//        >>>>>>>>>>>>>>>
+
+        digitalWrite(3, HIGH);
+        delay(5);
+        digitalWrite(3, LOW);
+        delay(5);
+
+        digitalWrite(5, HIGH);
+        delay(5);
+        digitalWrite(5, LOW);
+        delay(5);
+
+        digitalWrite(7, HIGH);
+        delay(5);
+        digitalWrite(7, LOW);
+        delay(5);
+
+        digitalWrite(9, HIGH);
+        delay(5);
+        digitalWrite(9, LOW);
+        delay(5);
+
+        digitalWrite(11, HIGH);
+        delay(5);
+        digitalWrite(11, LOW);
+        delay(5);
+
+        digitalWrite(13, HIGH);
+        delay(5);
+        digitalWrite(13, LOW);
+        delay(5);
+        
+    }
